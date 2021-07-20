4842c5efd564507b0c3d7ccee05cd78e96a0e703
Julian <biscombe.j@gmail.com>
2021-07-20 10:36:55 +0200

    changes

---
+++ src/endUser/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className="bg-red-600 text-6xl">
+            hasdfasdf
+        </div>
+    )
+}
+
+export default Dashboard
